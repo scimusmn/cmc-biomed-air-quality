73141e5e535db062d51b5b5f6f67766af8b2d7be
sanine <sanine.not@pm.me>
2024-02-20 10:56:05 -0600

    fix typescript eslint

--- packages/backend/.eslintrc.js
+++ packages/backend/.eslintrc.js
@@ -1,26 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: 'tsconfig.json',
+  },
+  rules: {
+  },
+};
--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const { request } = require('node:https');
 
+function getLatLong(token: string, latitude: number, longitude: number) {
 
-function getLatLong(
-  token: string, latitude: number, longitude: number
-) {
-  
 }
