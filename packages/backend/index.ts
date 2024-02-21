b21b506ce761d4be4ec0064fe0cd643592bd1658
sanine <sanine.not@pm.me>
2024-02-21 16:09:04 -0600

    add getSites() function

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -2,9 +2,50 @@ require('dotenv').config();
 const https = require('node:https');
 const http = require('node:http');
 const url = require('node:url');
+const { parse } = require('csv-parse');
 
-function getLatLong(token: string, latitude: number, longitude: number) {
-  console.log(token);
+function getSites(): Promise<[typeof http.ClientRequest, string[][]]> {
+  return new Promise((resolve) => {
+    const u = new url.URL('https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/Monitoring_Site_Locations_V2.dat');
+    https.get(u, (res: typeof http.ClientRequest) => {
+      const records: string[][] = [];
+      const parser = parse({ delimiter: '|' });
+      res.pipe(parser);
+      parser.on('readable', () => {
+        let record: string[] = parser.read();
+        while (record !== null) {
+          records.push(record);
+          record = parser.read();
+        }
+      });
+      parser.on('error', (err: Error) => {
+        console.error(err.message);
+      });
+      parser.on('end', () => {
+        resolve([res, records]);
+      });
+    });
+  });
+}
+
+interface Observation {
+  DateObserved: string;
+  HourObserved: number;
+  LocalTimeZone: string;
+  ReportingArea: string;
+  StateCode: string;
+  Latitude: number;
+  Longitude: number;
+  ParameterName: string;
+  AQI: number;
+  Category: { Number: number; Name: string };
+}
+
+function getLatLong(
+  token: string,
+  latitude: number,
+  longitude: number,
+): Promise<[typeof http.ClientRequest, [Observation]]> {
   return new Promise((resolve) => {
     const u = new url.URL('https://www.airnowapi.org/aq/observation/latLong/current/');
     u.searchParams.append('latitude', latitude);
@@ -26,4 +67,7 @@ function getLatLong(token: string, latitude: number, longitude: number) {
   });
 }
 
-getLatLong(process.env.API_KEY as string, 45, -90).then(([_, obj]) => console.log(obj));
+getLatLong(process.env.API_KEY as string, 45, -90).then(([_, obj]) => {
+  obj.forEach((x) => console.log(x.ParameterName, x.Latitude, x.Longitude));
+});
+// getSites().then(([_, records]) => console.log(records[0]));
