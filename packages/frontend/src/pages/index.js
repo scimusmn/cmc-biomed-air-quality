7b4949b073749dd0219471828a018a5b56ebb068
Trygve Nordberg <tnordberg@smm.org>
2024-05-07 15:11:35 -0500

    clean: remove unused frontend code
    This code has migrated to the backend/background process

--- packages/frontend/src/pages/index.js
+++ packages/frontend/src/pages/index.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import Map from '@components/Map';
-
-function loadGeo(name, url) {
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => [name, data]);
-}
-
-const geoDataSources = [
-  ['roads', '/gis/roads.geojson'],
-  ['cities', '/gis/places.geojson'],
-];
+import Home from '../components/Home';
 
 function IndexPage() {
-  const [mapShapes, setMapShapes] = React.useState(
-    geoDataSources.reduce((acc, [name, _]) => {
-      acc[name] = [];
-      return acc;
-    }, {}),
-  );
-
-  React.useEffect(() => {
-    Promise.all(geoDataSources.map(([name, url]) => loadGeo(name, url)))
-      .then((allShapes) => {
-        const newShapes = {};
-        allShapes.forEach(([name, data]) => {
-          newShapes[name] = data.features;
-          console.log(name, data.features, newShapes);
-        });
-        setMapShapes({ ...newShapes });
-      });
-  }, []);
-
   return (
-    <Map mapShapes={mapShapes} />
+    <Home />
   );
 }
 
