00128a0335adf8f4884df045d2185e303a6ab196
Ryan Bridge <ryan@softwareforgood.com>
2024-05-14 16:55:19 -0400

    fake scrubber in place

--- packages/frontend/src/components/Home/index.js
+++ packages/frontend/src/components/Home/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/media-has-caption */
+import React, { useEffect, useRef } from 'react';
 
 function Home() {
+  const videoRef = useRef(null);
+  const progressRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const progress = progressRef.current;
+
+    const updateProgress = () => {
+      const value = Math.round((video.currentTime / video.duration) * 100);
+      progress.value = value;
+    };
+
+    video.addEventListener('timeupdate', updateProgress);
+
+    return () => video.removeEventListener('timeupdate', updateProgress);
+  }, []);
+
   return (
     <div className="wrap">
       <div className="header">
@@ -9,7 +27,6 @@ function Home() {
 
       <div className="content-wrap">
         <div className="left-col">
-
           <div className="toggles">
             Toggles
           </div>
@@ -17,11 +34,19 @@ function Home() {
           <div className="legend">
             Legend
           </div>
-
         </div>
 
         <div className="right-col">
-          right col
+
+          <figure>
+            <video autoPlay muted loop id="video" ref={videoRef}>
+              <source src="/map-assets/one-day-loop.mp4" type="video/mp4" />
+            </video>
+            <figcaption>
+              <progress id="progress" max="100" value="0" ref={progressRef}>Progress</progress>
+            </figcaption>
+          </figure>
+
         </div>
       </div>
     </div>
