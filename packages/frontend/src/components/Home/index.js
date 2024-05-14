c76207ca8fdf455cd1b4e7919e12df6404eed8a5
Ryan Bridge <ryan@softwareforgood.com>
2024-05-14 14:29:32 -0400

    rough in boxes for layout

--- packages/frontend/src/components/Home/index.js
+++ packages/frontend/src/components/Home/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Start your app here.</p>
+    <div className="wrap">
+      <div className="header">
+        <h1>Interactive Map of Regional Air Quality</h1>
+      </div>
+
+      <div className="content-wrap">
+        <div className="left-col">
+
+          <div className="toggles">
+            Toggles
+          </div>
+
+          <div className="legend">
+            Legend
+          </div>
+
+        </div>
+
+        <div className="right-col">
+          right col
+        </div>
+      </div>
     </div>
   );
 }
