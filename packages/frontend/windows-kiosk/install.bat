da98bb5c4b1fcfeff55622516bbe08e4489278d7
Trygve Nordberg <tnordberg@smm.org>
2024-05-27 17:12:10 -0500

    feat: use relative pathing for future re-usability

--- packages/frontend/windows-kiosk/install.bat
+++ packages/frontend/windows-kiosk/install.bat
@@ -1,10 +1,11 @@
-# This installation script will place and configure the launch files inside the Windows startup folder
+@echo off
+cd /d "%~dp0"
 
-# Copy launch-kiosk.bat to startup folder
-copy /Y "C:\Users\exhibits\Documents\SMM\Dev\cmc-biomed-air-quality\packages\frontend\windows-kiosk\launch-kiosk.bat" "C:\Users\exhibits\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\launch-kiosk.bat"
+REM Copy launch-kiosk.bat to startup folder
+copy /Y "launch-kiosk.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\launch-kiosk.bat"
 
-# Copy startup.bat to startup folder
-copy /Y "C:\Users\exhibits\Documents\SMM\Dev\cmc-biomed-air-quality\packages\frontend\windows-kiosk\startup.bat" "C:\Users\exhibits\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
+REM Copy startup.bat to startup folder
+copy /Y "startup.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
 
-# Open the Startup folder to confirm startup files are in the correct location
-explorer "C:\Users\exhibits\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+REM Open the Startup folder to confirm startup files are in the correct location
+explorer "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
