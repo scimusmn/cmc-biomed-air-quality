e3b4a36cd9a318fe6a3686dd8dbb014e052827fa
Trygve Nordberg <tnordberg@smm.org>
2024-05-27 16:57:39 -0500

    feat: add kiosk startup scripts

---
+++ packages/frontend/windows-kiosk/install.bat
@@ -0,0 +1,5 @@
+# This should be placed in the parent directory of the source code repository
+# Double-click this file after making content edits to rebuild the application with new content
+
+cd ./tpt-skillsville-kiosk
+npm run rebuild-kiosk
