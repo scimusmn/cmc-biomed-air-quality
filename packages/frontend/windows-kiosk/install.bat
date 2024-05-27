adf920277f52f54fc0f65eb8f95c435dd63f46f5
Trygve Nordberg <tnordberg@smm.org>
2024-05-27 17:37:45 -0500

    feat: use shortcuts in place of direct .bat files

--- packages/frontend/windows-kiosk/install.bat
+++ packages/frontend/windows-kiosk/install.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
-REM Copy launch-kiosk.bat to startup folder
-copy /Y "launch-kiosk.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\launch-kiosk.bat"
+REM Create shortcut for stele-start.bat in the Startup folder with minimized window
+powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\stele-start.lnk'); $s.TargetPath='%~dp0stele-start.bat'; $s.WindowStyle=7; $s.Save()"
 
-REM Copy startup.bat to startup folder
-copy /Y "startup.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
+REM Create shortcut for startup.bat in the Startup folder with minimized window
+powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup.lnk'); $s.TargetPath='%~dp0startup.bat'; $s.WindowStyle=7; $s.Save()"
 
-REM Open the Startup folder to confirm startup files are in the correct location
+REM Open the Startup folder to confirm shortcuts are in the correct location
 explorer "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
